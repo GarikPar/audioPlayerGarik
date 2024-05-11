415e3a027af702150719a7a398975b63e4b39b3a
GarikPar <garik.paramazyan06@gmail.com>
2024-05-11 13:23:44 +0400

    Added music and data

---
+++ script.js
@@ -0,0 +1,10 @@
+let data = {
+    title: ["Fairytale"],
+    song: ["music/Alexander Rybak - Fairytale - LIVE _ Norway 🇳🇴 _ Grand Final _ Eurovision 2009.mp4",
+           "music/Eminem - Mockingbird (Lyrics).mp3",
+           "music/Tom Odell - Another Love (Lyrics).mp3"],
+    poster: ["https://live.staticflickr.com/2744/4133258502_6f45f4a896_z.jpg",
+            "https://i.pinimg.com/originals/6d/85/ff/6d85ff425c6552d5e05c27108ee899a5.jpg",
+            "https://m.media-amazon.com/images/I/61vsWhHWc5L._AC_UF894,1000_QL80_.jpg"],
+
+}
