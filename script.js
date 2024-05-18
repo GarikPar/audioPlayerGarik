b5ebbccebbbb4766852016b4ec388525cab02cbc
GarikPar <garik.paramazyan06@gmail.com>
2024-05-18 13:22:23 +0400

    Final object

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let data = {
-    title: ["Fairytale"],
+    title: ["Fairytale",
+             "Mockingbird",
+              "Another Love" ],
     song: ["music/Alexander Rybak - Fairytale - LIVE _ Norway 🇳🇴 _ Grand Final _ Eurovision 2009.mp4",
            "music/Eminem - Mockingbird (Lyrics).mp3",
            "music/Tom Odell - Another Love (Lyrics).mp3"],
@@ -41,10 +43,85 @@ function playOrPauseSong(){
        }
 }
 
+
 song.addEventListener("timeupdate", function(){
        let fill = document.getElementsByClassName("fill")[0]
-
+       
        let position = (song.currentTime/song.duration)*99
-
+       
        fill.style.width = position + "%"
+       convertTime(song.currentTime)
+
+       if(song.ended){
+              next
+       }
 })
+
+function convertTime(seconds){
+       let currentTime  = document.getElementsByClassName("currentTime")[0]
+       // currentTime.textContent=seconds+" /" + song.duration
+
+       let min = Math.floor(seconds/60)
+
+       let sec = Math.floor(seconds%60)
+
+       min = (min<10) ? "0"+min: min
+       sec = (sec<10) ? "0"+sec: sec
+
+       let durMin = Math.floor(song.duration/60)
+
+       let durSec = Math.floor(song.duration%60)
+       durMin = (durMin<10) ? "0"+durMin: durMin
+       durSec = (durSec<10) ? "0"+durSec: durSec
+       currentTime.textContent=min + " : " + sec + " / " + durMin + ":" + durSec
+}
+
+function prev(){
+       currentSong --
+       play  = document.getElementById("play")
+
+       if(currentSong < 0){
+              currentSong = data.song.length - 1
+       }
+
+       playSong()
+       play.src = "images/pause.png"
+}
+
+function next(){
+       currentSong ++
+       play  = document.getElementById("play")
+
+       if(currentSong > data.song.length-1){
+              currentSong = 0
+       }
+
+       playSong()
+       play.src = "images/pause.png"
+}
+
+function mute(){
+       let mute = document.getElementById("mute")
+       if (song.muted){
+              song.muted = false
+              mute.src = "images/volume.png"
+       }else{
+              song.muted  = true
+              mute.src  = "images/volume-mute.png"
+       }
+}
+function decrease(){
+       let mute = document.getElementById("mute")
+       song.volume-=0.2
+       if(song.volume<=0.1){
+              mute.src = "images/volume-mute.png"
+       }
+}
+function increase(){
+       let mute = document.getElementById("mute")
+       song.volume+=0.2
+       if(song.volume>0.1){
+              mute.src = "images/volume.png"
+       }
+
+}
